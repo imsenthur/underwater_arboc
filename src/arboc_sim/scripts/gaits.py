79c2bcbfa17cee31cd527b11a78273b1608bf050
imsenthur <imsenthur@gmail.com>
2018-07-23 01:17:02 +0530

    simulation pkg of arboc.

---
+++ src/arboc_sim/scripts/gaits.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+"""
+The node publishes joint position commands to effort position controllers.
+The controllers should already be spawned and running and communicating
+with the appropriate robot hardware interface.
+"""
+
+import rospy
+from std_msgs.msg import Float64
+
+import numpy as np
+
+class JointCmds:
+
+    def __init__( self, num_mods ) :
+        
+        self.num_modules = num_mods
+        self.jnt_cmd_dict = {}        
+        self.joints_list = []
+        self.t = 0.0
+        for i in range(1,self.num_modules) :
+            leg_str='joint_'
+            if i < 10 :
+                leg_str += '0' + str(i)
+            else :
+                leg_str += str(i)
+            self.joints_list += [leg_str]
+
+    def update( self, dt ) :
+
+        self.t += dt
+        a = 0.8*np.pi
+        b = 2*np.pi
+        c = 1*np.pi
+
+        num_segments = 8
+        gamma=-c/num_segments
+        beta=b/num_segments
+        alpha=a*np.abs(np.sin(beta/2))
+
+        for i, jnt in enumerate(self.joints_list):
+            if i%2==0:
+                self.jnt_cmd_dict[jnt] = alpha*np.sin(self.t*np.pi - 0.5*i*beta)+gamma
+                #rospy.loginfo(str(self.t))
+                #self.jnt_cmd_dict[jnt] = alpha*np.sin(2*self.t*np.pi+(i-8)*beta)+gamma
+            else:
+                self.jnt_cmd_dict[jnt] = alpha*np.sin(self.t*np.pi - 0.5*(i+1)*beta)+gamma
+                #rospy.loginfo(str(self.t))
+                #self.jnt_cmd_dict[jnt] = alpha*np.sin(2*self.t*np.pi+(i-8)*beta)+gamma       
+
+        return self.jnt_cmd_dict
+
+
+def publish_commands( num_modules, hz ):
+    pub={}
+    ns_str = '/arboc'
+    cont_str = 'pc'
+    for i in range(1,num_modules) :
+        leg_str='joint_'
+        if i < 10 :
+            leg_str += '0' + str(i)
+        else :
+            leg_str += str(i)
+        pub[leg_str] = rospy.Publisher( ns_str + '/' + leg_str + '_'
+                                        + cont_str + '/command',
+                                        Float64, queue_size=10 )
+
+    rospy.init_node('controller', anonymous=True)
+    rate = rospy.Rate(hz)
+    jntcmds = JointCmds(num_mods=num_modules)
+    while not rospy.is_shutdown():
+        jnt_cmd_dict = jntcmds.update(1./hz)
+        for jnt in jnt_cmd_dict.keys() :
+            pub[jnt].publish( jnt_cmd_dict[jnt] )
+        rate.sleep()
+
+
+if __name__ == "__main__":
+    try:
+        num_modules = 8       
+        hz = 100
+        publish_commands( num_modules, hz )
+    except rospy.ROSInterruptException:
+        pass
---
+++ src/arboc_sim/scripts/joint_publisher.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Float64
+
+class JointPub(object):
+    def __init__(self):
+
+        self.publishers_array = []
+        self.joint_01 = rospy.Publisher('/arboc/joint_01_pc/command', Float64, queue_size=1)
+        self.joint_02 = rospy.Publisher('/arboc/joint_02_pc/command', Float64, queue_size=1)
+        self.joint_03 = rospy.Publisher('/arboc/joint_03_pc/command', Float64, queue_size=1)
+        self.joint_04 = rospy.Publisher('/arboc/joint_04_pc/command', Float64, queue_size=1)
+        self.joint_05 = rospy.Publisher('/arboc/joint_05_pc/command', Float64, queue_size=1)
+        self.joint_06 = rospy.Publisher('/arboc/joint_06_pc/command', Float64, queue_size=1)
+        self.joint_07 = rospy.Publisher('/arboc/joint_07_pc/command', Float64, queue_size=1)
+        
+        self.publishers_array.append(self.joint_01)
+        self.publishers_array.append(self.joint_02)
+        self.publishers_array.append(self.joint_03)
+        self.publishers_array.append(self.joint_04)
+        self.publishers_array.append(self.joint_05)
+        self.publishers_array.append(self.joint_06)
+        self.publishers_array.append(self.joint_07)
+
+    def move_joints(self, joints_array):
+   
+        i=0
+        for publisher in self.publishers_array:
+          joint_value = Float64()
+          joint_value.data=joints_array[i]
+          rospy.loginfo(str(joint_value)+" on "+str((publisher.name).split('/')[2]) + " @rate: " + str(rate_value))    
+          self.joint_01.publish(joint_value)
+          self.joint_02.publish(joint_value)
+          publisher.publish(joint_value)
+          i = i+1
+        rospy.loginfo("*************************************************************************************") 
+
+
+    def start_loop(self, rate_value = 2.0):
+        rospy.loginfo("Starting Control")
+        pos1 = [-1.57,1.57,-1.57,1.57,-1.57,1.57,-1.57]
+        pos2 = [1.57,-1.57,1.57,-1.57,1.57,-1.57,1.57]
+        position = "pos1"
+        rate = rospy.Rate(rate_value)
+        while not rospy.is_shutdown():
+          if position == "pos1":
+            self.move_joints(pos1)
+            position = "pos2"
+          else:
+            self.move_joints(pos2)
+            position = "pos1"
+          rate.sleep()
+
+
+if __name__=="__main__":
+
+  try:
+    rospy.init_node('joint_publisher_node')
+    joint_publisher = JointPub()
+
+    #** tweak the publication rate **#
+    rate_value = 0.5
+    joint_publisher.start_loop(rate_value)
+
+  except rospy.ROSInterruptException:
+    pass
